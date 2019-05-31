a01f8c5f3d790256e91a7545286ae62a61c4735c
natxtw <36550708+natxtw@users.noreply.github.com>
2019-05-31 23:28:40 +0100

    Basic Shooting.
    Ability to shoot and detect enemy, Must have the ShootingGunTarget script on the enemy if they are going to be killed.

---
+++ Cluster Tower Defence Project/Assets/First Commit/ShootingGun.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingGun : MonoBehaviour
+{
+    [SerializeField] private float Damage = 10.0f;
+    [SerializeField] private float Range = 200.0f;
+
+    public Camera RaycastChecker;
+    public ParticleSystem Flash;
+
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    private void Fire() 
+    {
+        Flash.Play(); 
+
+        RaycastHit HitInfo;
+
+        if (Physics.Raycast(RaycastChecker.transform.position, RaycastChecker.transform.forward, out HitInfo, Range))
+        // Var 1 - Shoots out a Ray at the position of the camera. Var 2 - Finds the direction you are facing. 
+        // Var 3 - Gathers the info and places it into the hitinfo variable. Var 4 - Inputs the range to prevent endless hitting.
+        {
+            ShootingGunTarget target = HitInfo.transform.GetComponent<ShootingGunTarget>();
+            if (target != null)
+            {
+                target.DamageTaken(Damage);
+            }
+            Debug.Log(HitInfo.transform.name); // Displays the object it hits in the console.
+        }
+    }
+}
---
+++ Cluster Tower Defence Project/Assets/First Commit/ShootingGunTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingGunTarget : MonoBehaviour
+{
+    [SerializeField] private float Health = 50.0f; 
+
+    public void DamageTaken(float AmountOfDamageTaken)
+    {
+        Health -= AmountOfDamageTaken;
+        if (Health <= 0)
+        {
+            HasDied();
+        }
+    }
+
+    public void HasDied()
+    {
+        Destroy(gameObject);
+    }
+}
+
